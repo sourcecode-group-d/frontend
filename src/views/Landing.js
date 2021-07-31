21bcb652a852f3c1b3655d5e2dca2bf0c560820c
saedjabali <saad.jabali@yahoo.com>
2021-07-31 12:55:01 +0300

    inital design

---
+++ src/views/Landing.js
@@ -0,0 +1,15 @@
+import React,{Fragment} from "react";
+
+class Landing extends React.Component{
+    render() {
+        return(
+            <Fragment>
+                <div>
+                    hihihi
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Landing;
